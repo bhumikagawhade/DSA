c87ed605c0964b492590576fda6592428e2a9f33
Bhumika04 <bhumikagawhade04@gmail.com>
2026-08-01 00:09:56 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (88.38%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans ="";
+
+        int i=s.length()-1;
+
+        while(i>=0){
+            while(i>=0 && s[i]==' '){
+                i--;
+            }
+            if(i<0){
+                break;
+            }
+
+            int j=i;
+
+            while(j>=0 && s[j] !=' '){
+                j--;
+
+            }
+            ans+=s.substr(j+1,i-j);
+
+            //remove space
+            while(j>=0 && s[j]==' '){
+                j--;
+            }
+
+            if(j>=0){
+                ans+=' ';
+            }
+
+            i=j;
+
+
+        }
+
+        return ans;
+    }
+};
